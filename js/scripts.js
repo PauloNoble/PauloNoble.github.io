d62d237b08b96c798eaff4f96797e41f4fb798f1
Paulo Noble <noblepaulo@gmail.com>
2023-02-25 02:43:10 -0300

    Proyecto finalizado se agrego boton de descarga de cv, boton de scroll to top

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,45 @@
 
 const BTNSWITCH = document.querySelector('#switch');
-
+//Funcioón modo nocturno
 BTNSWITCH.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     BTNSWITCH.classList.toggle('active');
 });
+
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+
+let mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; // Para Safari
+  document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE and Opera
+}
